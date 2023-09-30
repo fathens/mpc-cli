953b1cbc62828e210237767d05863c4215c47c00
kunio <kunio@sawatani.jp>
2023-09-30 12:14:31 +0900

    Impl sha512_256i tagged version

--- crates/common/src/hash.rs
+++ crates/common/src/hash.rs
@@ -1,5 +1,5 @@
 use crate::fixed_bytes::{fixed_bytes, FixedBytes};
-use num_bigint::BigUint;
+use num::{BigUint, Zero};
 use sha2::{Digest, Sha512_256};
 
 const HASH_INPUT_DELIMITER: [u8; 1] = ['$' as u8];
@@ -8,8 +8,13 @@ const HASH_INPUT_DELIMITER: [u8; 1] = ['$' as u8];
 pub struct Hash256([u8; 32]);
 fixed_bytes!(Hash256);
 
-fn sha512_256(src_list: &[&[u8]]) -> Hash256 {
+fn sha512_256(tag: Option<&[u8]>, src_list: &[&[u8]]) -> Hash256 {
     let mut hasher = Sha512_256::new();
+    if let Some(tag) = tag {
+        let hashed_tag = sha512_256(None, &[tag]);
+        hasher.update(hashed_tag.as_ref());
+        hasher.update(hashed_tag);
+    }
     let in_len_bz = (src_list.len() as u64).to_le_bytes();
     hasher.update(&in_len_bz);
     for src in src_list {
@@ -21,23 +26,33 @@ fn sha512_256(src_list: &[&[u8]]) -> Hash256 {
     Hash256(hasher.finalize().into())
 }
 
-fn sha512_256i(src_list: &[BigUint]) -> Hash256 {
-    let bs_list: Vec<_> = src_list.iter().map(|x| x.to_bytes_be()).collect();
+fn sha512_256i(tag: Option<&[u8]>, src_list: &[BigUint]) -> Hash256 {
+    let bs_list: Vec<_> = src_list
+        .iter()
+        .map(|x| if x.is_zero() { vec![] } else { x.to_bytes_be() })
+        .collect();
     let bss: Vec<_> = bs_list.iter().map(|x| x.as_slice()).collect();
-    sha512_256(bss.as_slice())
+    sha512_256(tag, bss.as_slice())
 }
 
 macro_rules! sha512_256 {
     ($($src:expr),+) => {{
         let src_list = [$($src),+];
-        sha512_256(&src_list)
+        sha512_256(None, &src_list)
     }};
 }
 
 macro_rules! sha512_256i {
     ($($src:expr),+) => {{
         let src_list = [$($src),+];
-        sha512_256i(&src_list)
+        sha512_256i(None, &src_list)
+    }};
+}
+
+macro_rules! sha512_256i_tagged {
+    ($tag:expr, $($src:expr),+) => {{
+        let src_list = [$($src),+];
+        sha512_256i(Some($tag), &src_list)
     }};
 }
 
@@ -82,6 +97,55 @@ mod tests {
                 255, 179, 57, 7, 138, 28, 226, 9, 15, 169, 24, 135, 190, 32
             ],
             two.0
+        );
+        let three = sha512_256i!(
+            BigUint::from(12345678_u32),
+            BigUint::from(0_u32),
+            BigUint::from(34567890_u32)
+        );
+        assert_eq!(
+            [
+                139, 229, 38, 79, 150, 188, 146, 98, 69, 214, 76, 111, 80, 122, 155, 236, 73, 128,
+                40, 100, 24, 163, 191, 55, 178, 177, 13, 12, 133, 150, 138, 209
+            ],
+            three.0
+        );
+    }
+
+    #[test]
+    fn hash_bigint_tagged() {
+        let one = sha512_256i_tagged!("tag-a".as_bytes(), BigUint::from(12345678_u32));
+        assert_eq!(
+            [
+                62, 229, 129, 172, 169, 125, 219, 131, 105, 95, 195, 233, 170, 196, 197, 213, 236,
+                163, 114, 155, 156, 196, 165, 198, 67, 235, 246, 30, 140, 248, 88, 95
+            ],
+            one.0
+        );
+        let two = sha512_256i_tagged!(
+            "tag-b".as_bytes(),
+            BigUint::from(12345678_u32),
+            BigUint::from(34567890_u32)
+        );
+        assert_eq!(
+            [
+                27, 182, 159, 219, 92, 228, 45, 221, 84, 231, 52, 154, 154, 33, 20, 84, 83, 190,
+                12, 89, 205, 95, 64, 217, 176, 132, 5, 157, 75, 168, 73, 38
+            ],
+            two.0
+        );
+        let three = sha512_256i_tagged!(
+            "tag-c".as_bytes(),
+            BigUint::from(12345678_u32),
+            BigUint::from(0_u32),
+            BigUint::from(34567890_u32)
+        );
+        assert_eq!(
+            [
+                213, 63, 56, 234, 196, 75, 69, 74, 68, 71, 105, 213, 75, 149, 4, 237, 211, 185, 20,
+                151, 149, 84, 187, 218, 108, 208, 171, 58, 202, 185, 168, 189
+            ],
+            three.0
         );
     }
 }
