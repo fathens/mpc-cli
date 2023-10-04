a8c0631cf227a4b2f0004c0d829998ce08dd8b5b
kunio <kunio@sawatani.jp>
2023-10-04 22:07:27 +0900

    Impl `get_random_int`

--- crates/common/src/error.rs
+++ crates/common/src/error.rs
@@ -1,4 +1,6 @@
-#[derive(Debug)]
+use std::ops::RangeInclusive;
+
+#[derive(Debug, PartialEq)]
 pub struct CommonError(String);
 
 impl CommonError {
@@ -8,5 +10,13 @@ impl CommonError {
 
     pub fn division_by_zero() -> CommonError {
         CommonError("Division by zero".to_owned())
+    }
+
+    pub fn invalid_random_bits_length<N>(value: N, range: RangeInclusive<N>) -> CommonError
+    where
+        N: std::fmt::Display + std::fmt::Debug,
+    {
+        let msg = format!("Invalid random bits length: given {value} not in {range:?}",);
+        CommonError(msg)
     }
 }
