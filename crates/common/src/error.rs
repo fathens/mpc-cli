77e4016309b78a9dc88004194d0de57c151661b5
kunio <kunio@sawatani.jp>
2023-10-05 22:33:14 +0900

    Impl all functions of random

--- crates/common/src/error.rs
+++ crates/common/src/error.rs
@@ -12,11 +12,19 @@ impl CommonError {
         CommonError("Division by zero".to_owned())
     }
 
-    pub fn invalid_random_bits_length<N>(value: N, range: RangeInclusive<N>) -> CommonError
+    pub fn out_of_range<N>(value: N, range: RangeInclusive<N>) -> CommonError
     where
         N: std::fmt::Display + std::fmt::Debug,
     {
-        let msg = format!("Invalid random bits length: given {value} not in {range:?}",);
+        let msg = format!("Out of range: given {value} not in {range:?}",);
+        CommonError(msg)
+    }
+
+    pub fn invalid_argument<A>(value: A, msg: &str) -> CommonError
+    where
+        A: std::fmt::Display + std::fmt::Debug,
+    {
+        let msg = format!("Invalid argument: {value}: {msg}",);
         CommonError(msg)
     }
 }
